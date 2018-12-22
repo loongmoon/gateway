421415a84a9efc5f2d39c06957e4aed9f6df6fec
zhaobao <zhaobao@tiansishangwu.com>
2018-12-23 04:12:36 +0800

    @EnableEurekaClient

--- src/main/java/com/zhao/gateway/GatewayApplication.java
+++ src/main/java/com/zhao/gateway/GatewayApplication.java
@@ -3,12 +3,12 @@ package com.zhao.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @Slf4j
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
